747b9511ffa888b1145fd2b6126dfa61c6d5032f
adaminthecape <adaminthecape@gmail.com>
2025-03-21 02:23:49 +0200

    Add MUI fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/inter';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
