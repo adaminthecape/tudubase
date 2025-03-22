2a10bacfe2fa66ca166cadb94455766bed5ae2bc
adaminthecape <adaminthecape@gmail.com>
2025-03-22 17:45:07 +0200

    Add character page & fix app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "tudu",
+  description: "A gamified task manager",
 };
 
 const geistSans = Geist({
---
+++ app/user/tasks/character/page.tsx
@@ -0,0 +1,17 @@
+/**
+ * Main Character Page
+ *
+ * This page displays the character information and equipment.
+ * This includes stats, inventory, and other character-specific information.
+ */
+
+import TaskMessagesView from "@/components/tasks/TaskMessagesView";
+
+export default function TasksPage()
+{
+	return (
+		<div className="w-full">
+			<TaskMessagesView />
+		</div>
+	);
+}
