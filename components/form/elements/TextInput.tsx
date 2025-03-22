cefa053555d19bbd6d546380f75b529d06c35cb0
adaminthecape <adaminthecape@gmail.com>
2025-03-22 17:14:58 +0200

    Extend generic input for Text and Number inputs

---
+++ components/form/elements/TextInput.tsx
@@ -0,0 +1,26 @@
+import { GenericInputProps, GenericInput } from "./GenericInput";
+
+export default function TextInput({
+	field,
+	value,
+	updateValue,
+	updateError,
+	i18n,
+}: GenericInputProps<string | number>)
+{
+	const i18nVars: GenericInputProps<any>['i18n'] = {
+		...(i18n || {}),
+		label: field.label || i18n?.label || field.key || 'Input',
+	};
+
+	return (
+		<GenericInput
+			aria-label={i18n?.label || field.key || 'Input'}
+			i18n={i18nVars}
+			field={field}
+			value={value}
+			updateValue={updateValue}
+			updateError={updateError}
+		/>
+	);
+}
