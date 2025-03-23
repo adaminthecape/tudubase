566e8f2067dcfab12754d197a2e868f5f60fc8a3
adaminthecape <adaminthecape@gmail.com>
2025-03-23 03:05:01 +0200

    Add types for Messages view

---
+++ components/tasks/types.tsx
@@ -0,0 +1,51 @@
+import { Collection } from '@/zencore/arch/Collection';
+import { Item, Task, TaskActivity } from '@/zencore/ItemTypes';
+import { TaskMaster } from '@/zencore/arch/TaskMaster';
+
+/**
+ * Collection list: to collect for each Collection:
+ * - Collection data
+ * - Whether a Task in it is active
+ * - Whether a Task in it is due soon
+ * - Last activity (especially how long ago & latest message)
+ * - Whether a TaskMaster is assigned
+ */
+export type CollectionListItem = Item<Collection> & {
+	collectionId: string;
+	hasActiveTasks?: boolean;
+	hasDueTasks?: boolean;
+	lastActivity?: Item<TaskActivity>;
+	taskMaster?: Item<TaskMaster>;
+}
+
+export type TaskCollectionListProps = {
+	listItems: CollectionListItem[];
+	setSelectedItem: (item: CollectionListItem) => void;
+	selectedItemId?: string;
+};
+
+export type TaskCollectionListItemProps = CollectionListItem & {
+	setSelectedItem: (item: CollectionListItem) => void;
+	selectedItemId?: string;
+};
+
+/**
+ * List of TaskActivity items
+ * Needs to display the info about the activity, and the Task it belongs to.
+ * Multiple activities under the same Task will be grouped, so we need to
+ * display the relevant Task or TaskMaster fewer times.
+ * For efficiency, we will just group all the Items together in the props array.
+ */
+export type TaskActivityListItem = (
+	Item<Task> |
+	Item<TaskMaster> |
+	Item<TaskActivity>
+)
+
+export type TaskActivityListProps = {
+	collection: CollectionListItem | undefined;
+};
+
+export type TaskActivityListHeaderProps = {
+	collection: CollectionListItem | undefined;
+};
