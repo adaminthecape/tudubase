650298ec78e587b6404b0a7d18ab269d598dcd71
adaminthecape <adaminthecape@gmail.com>
2025-03-25 17:52:57 +0200

    Remove unused testing code

--- app/user/tasks/page.tsx
+++ app/user/tasks/page.tsx
@@ -10,18 +10,6 @@ import TaskMessagesView from "@/components/tasks/TaskMessagesView";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-import { getItemHandler } from '@/api/ItemUtils';
-import { ItemTypes } from '@/zencore/ItemTypes';
-import { Uuid } from '@/zencore/Utils';
-
-async function getCollections()
-{
-	const handler = getItemHandler({
-		itemType: ItemTypes.Collection,
-		id: Uuid.generateUuid(),
-	});
-}
-
 export default async function TasksPage()
 {
 	const supabase = await createClient()
