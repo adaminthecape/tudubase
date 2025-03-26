d25a7dde682eced3b396fb85271e0adea85e50b9
adaminthecape <adaminthecape@gmail.com>
2025-03-26 21:26:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,8 @@ export default async function RootLayout({
 		<html lang="en" className={geistSans.className} suppressHydrationWarning>
 		<body className="bg-background text-foreground">
 			<InitColorSchemeScript />
+			<main className="min-h-screen flex flex-col items-center">
+				<div className="flex-1 w-full flex flex-col items-center">
 			<CssVarsProvider>
 			<ThemeProvider
 				attribute="class"
@@ -45,8 +47,6 @@ export default async function RootLayout({
 				enableSystem
 				disableTransitionOnChange
 			>
-			<main className="min-h-screen flex flex-col items-center">
-				<div className="flex-1 w-full flex flex-col items-center">
 				<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
 					<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
 						<div className="flex gap-2 items-start font-semibold" style={{alignItems: 'center'}}>
@@ -79,6 +79,15 @@ export default async function RootLayout({
 									<I18N sid="nav.quests.title" />
 								</Button>
 							</Link>
+							<Link href={"/cache"}>
+								<Button
+									size="sm"
+									color="neutral"
+									variant="soft"
+								>
+									Cache
+								</Button>
+							</Link>
 						</div>
 					</div>
 					<div className="flex justify-end items-center gap-2">
@@ -103,12 +112,12 @@ export default async function RootLayout({
 							Supabase
 						</a>
 					</p>
-					<ThemeSwitcher />
+					{/* <ThemeSwitcher /> */}
 				</footer>
-				</div>
-			</main>
 			</ThemeProvider>
 			</CssVarsProvider>
+				</div>
+			</main>
 			</body>
 		</html>
 	);
