4456168a0175580287ac64f6be59798b1b1e767e
adaminthecape <adaminthecape@gmail.com>
2025-03-27 09:52:59 +0200

    Make TaskActivityList work properly

--- components/tasks/messages/TaskActivityList.tsx
+++ components/tasks/messages/TaskActivityList.tsx
@@ -1,34 +1,136 @@
-import * as React from 'react';
 import Box from '@mui/joy/Box';
 import Sheet from '@mui/joy/Sheet';
 import Stack from '@mui/joy/Stack';
-import AvatarWithStatus from '@/components/tasks/AvatarWithStatus';
-import MessageBubble from './MessageBubble';
 import MessageInput from './MessageInput';
 import TaskActivityListHeader from '@/components/tasks/messages/TaskActivityListHeader';
 import { TaskActivityListItem, TaskActivityListProps } from '@/components/tasks/types';
+import { Item, ItemTypes, Task, TaskActivity } from '@/zencore/ItemTypes';
+import { Utils } from '@/zencore/Utils';
+import { i18n } from '@/i18n';
+import { TaskMaster } from '@/zencore/arch/TaskMaster';
+import { Card, Typography } from '@mui/joy';
+import ItemEditButton from '@/components/itemForms/ItemEditButton';
+import { useEffect, useState } from 'react';
+import { addTaskToCollection, getCollectionAndTaskAndActivityData } from '../utils';
+
+function displayActivityListItem(item: Item<TaskActivity>, index: number)
+{
+	if(!item.activityType)
+	{
+		return (<div>Unknown</div>);
+	}
+
+	const i18nMessage = i18n.activityMessages[item.activityType];
+
+	return (
+		<div className="w-full flex flex-row justify-end">
+			<Typography
+				key={index}
+				color='success'
+				sx={{ fontSize: 'sm', fontWeight: 'sm' }}
+			>{i18nMessage}</Typography>
+		</div>
+	);
+}
+
+function displayTaskListItem(item: Item<Task>, index: number)
+{
+	return (
+		<Card
+			variant='plain'
+			className="mr-4"
+		>
+			<Typography>{item.title}</Typography>
+			<Typography sx={{ fontSize: 'xs' }}>{item.notes}</Typography>
+			<div className="w-full flex flex-row justify-end">
+				<ItemEditButton
+					itemType={ItemTypes.Task}
+					itemId={item.id}
+					initialValues={item}
+				/>
+			</div>
+		</Card>
+	);
+}
+
+function displayTaskMasterListItem(item: Item<TaskMaster>, index: number)
+{
+	return (<div key={index}>TaskMaster</div>);
+}
+
+function parseActivityListItem(listItem: TaskActivityListItem, index: number)
+{
+	if(listItem.typeId === ItemTypes.Task)
+	{
+		return displayTaskListItem(listItem as Item<Task>, index);
+	}
+	else if(listItem.typeId === ItemTypes.TaskActivity)
+	{
+		return displayActivityListItem(listItem as Item<TaskActivity>, index);
+	}
+	else if(listItem.typeId === ItemTypes.TaskMaster)
+	{
+		return displayTaskMasterListItem(listItem as Item<TaskMaster>, index);
+	}
+	else
+	{
+		return (<div>parseActivityListItem: Unknown {JSON.stringify(listItem)}</div>);
+	}
+}
 
 export default function TaskActivityList(props: TaskActivityListProps)
 {
 	const { collection } = props;
-	const [
-		chatMessages,
-		setChatMessages
-	] = React.useState<TaskActivityListItem[]>([]);
-	const [textAreaValue, setTextAreaValue] = React.useState('');
+	const [activityMessages, setActivityMessages] = useState<TaskActivityListItem[]>([]);
+	const [inputValues, setInputValues] = useState<Partial<Item<Task>>>({});
 
-	React.useEffect(() => {
-		setChatMessages([]);
+	async function selectCollection({ collectionId }: { collectionId: string })
+	{
+		const res = await getCollectionAndTaskAndActivityData({
+			collectionIds: [collectionId],
+		});
+
+		setActivityMessages([...res.activity, ...res.tasks].sort(
+			(a, b) => (a.createdAt ?? a.updatedAt ?? 0) - (b.createdAt ?? b.updatedAt ?? 0)
+		));
+	}
+
+	useEffect(() => 
+	{
+		if(collection?.collectionId)
+		{
+			selectCollection(collection);
+		}
 	}, [collection?.collectionId]);
+
+	async function submitMessage()
+	{
+		if(!Utils.isPopulatedObject(inputValues))
+		{
+			return;
+		}
+
+		if(!collection?.collectionId)
+		{
+			console.warn('No collection ID!');
+			return;
+		}
+
+		// submit the task data to the server
+		const addTaskRes = await addTaskToCollection({
+			collectionId: collection?.collectionId,
+			baseData: inputValues,
+		});
+	}
 
 	return (
 		<Sheet
-		sx={{
-			height: { xs: 'calc(80dvh - var(--Header-height))', md: '80dvh' },
-			display: 'flex',
-			flexDirection: 'column',
-			backgroundColor: 'background.level1',
-		}}
+			sx={{
+				height: { xs: 'calc(80dvh - var(--Header-height))', md: '80dvh' },
+				display: 'flex',
+				flexDirection: 'column',
+				backgroundColor: 'background.level1',
+			}}
 		>
 		<TaskActivityListHeader collection={collection} />
 		<Box
@@ -44,43 +146,23 @@ export default function TaskActivityList(props: TaskActivityListProps)
 			}}
 		>
 			<Stack spacing={2} sx={{ justifyContent: 'flex-end' }}>
-			{chatMessages?.map((message: TaskActivityListItem, index: number) => {
-				const isYou = message.createdBy === 'ca84b5a0-a0ae-425d-993a-4e63d235f222';
-				return (
-				<Stack
-					key={index}
-					direction="row"
-					spacing={2}
-					sx={{ flexDirection: isYou ? 'row-reverse' : 'row' }}
-				>
-					{isYou && (
-					<AvatarWithStatus
-						online={!!message.id}
-						src={'message'}
-					/>
-					)}
-					<MessageBubble variant={isYou ? 'sent' : 'received'} {...message} />
-				</Stack>
-				);
-			})}
-			</Stack>
-		</Box>
-		<MessageInput
-			textAreaValue={textAreaValue}
-			setTextAreaValue={setTextAreaValue}
-			onSubmit={() => {
-			const newId = chatMessages.length + 1;
-			const newIdString = newId.toString();
-			setChatMessages([
-				...chatMessages,
+				{activityMessages?.map((
+					listItem: TaskActivityListItem,
+					index: number
+				) =>
 				{
-					id: newIdString,
-					sender: 'You',
-					content: textAreaValue,
-					timestamp: 'Just now',
-				},
-			]);
-			}}
+					return (
+						<div key={`${`${listItem.id}-${index}`}`}>
+							{parseActivityListItem(listItem, index)}
+						</div>
+					);
+				})}
+			</Stack>
+			</Box>
+		<MessageInput
+			values={inputValues}
+			setValues={setInputValues}
+			onSubmit={submitMessage}
 		/>
 		</Sheet>
 	);
