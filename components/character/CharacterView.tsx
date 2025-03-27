30ae27b55fe30a8941c171c545fa8570a5935c1a
adaminthecape <adaminthecape@gmail.com>
2025-03-27 12:05:52 +0200

    Update CharacterView.tsx

--- components/character/CharacterView.tsx
+++ components/character/CharacterView.tsx
@@ -1,10 +1,5 @@
-import { loadItem, searchItems } from "@/api/actions/Generic";
-import { getFieldsForItemTypeSync } from "@/apiUtils/fieldUtils";
-import { DbFilter, DbFilterHandler, DbFilters } from "@/zencore/Filters";
-import { ItemTypes } from "@/zencore/ItemTypes";
 import { Stack } from "@mui/joy";
 import { SxProps } from "@mui/material";
-import { useEffect } from "react";
 
 export function CharacterView({
 	children,
@@ -14,25 +9,6 @@ export function CharacterView({
 	sx?: SxProps;
 })
 {
-	// get the character stats from the db
-	function getCharacterDetails()
-	{
-		console.log('getCharacterDetails');
-		// for now, one character per user, so let's just search for them all
-		searchItems({
-			itemType: ItemTypes.Character,
-			pagination: { page: 1, pageSize: 1 }
-		}).then(({ success, data }) =>
-		{
-			console.log({ success, data });
-			const character = data?.results?.[0];
-
-			console.log('character:', character);
-		});
-	}
-
-	useEffect(getCharacterDetails, []);
-
 	return (
 		<Stack
 			spacing={1}
